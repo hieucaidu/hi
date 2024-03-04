58848ed5689ee17618958b8e3eee071245afb50c
Thor_ĐiCùngA <153697284+hieucaidu@users.noreply.github.com>
2024-03-04 21:28:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'hiiiiiii';
+const giftUrl = 'hiiiiiiiii';
+const eventName = 'hiiiiiiiii';
+const titleCard = 'hiiiiiiiiii';
+const contentCard = 'hiiiiiiiiiiiii';
+
+
+const giftImage = 'anh-thor.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
